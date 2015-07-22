77d00fa86fae361291c984d3298515345037625d
Stoyan Konstantinov <stoyan.konstantinov@gmail.com>
2015-07-23 00:03:20 +0300

    some configs

--- dstorage_extension/settings.py
+++ dstorage_extension/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'office'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -86,7 +87,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'bg-bg'
 
 TIME_ZONE = 'UTC'
 
@@ -101,3 +102,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+    )
+
+LOGIN_REDIRECT_URL = '/office'
